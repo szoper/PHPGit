dc91105e2be699d3428fda964a0427f9cc7bddbb
Martijn van Maurik <martijn@vmaurik.nl>
2015-06-02 10:42:28 +0200

    No tar.bz2

--- src/PHPGit/Command/ArchiveCommand.php
+++ src/PHPGit/Command/ArchiveCommand.php
@@ -84,6 +84,6 @@ class ArchiveCommand extends Command
             ->setAllowedTypes('format', array('null', 'string'))
             ->setAllowedTypes('prefix', array('null', 'string'))
 
-            ->setAllowedValues('format', array('tar', 'zip', 'tar.gz', 'tgz', 'tar.bz2', 'tbz2'));
+            ->setAllowedValues('format', array('tar', 'zip', 'tar.gz', 'tgz'));
     }
 }
